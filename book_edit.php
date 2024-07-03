4662bc5e595591284aac2602935f8b6818c94e99
Jusaila <100611092+Jusaila@users.noreply.github.com>
2024-07-03 12:36:15 +0530

    Update book_edit.php

--- book_edit.php
+++ book_edit.php
@@ -61,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
-            background-image: url('images/library-filled-with-books.jpg'); /* Replace with your image file path */
+            background-image: url('images/library-filled-with-books.jpg');
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
